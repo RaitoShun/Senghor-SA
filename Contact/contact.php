5a0004e37d7e80fb73412117265d4cbf30ca497c
RaitoShun <abrahamthiao@gmail.com>
2019-12-03 15:43:48 +0200

    new nav highlight and new arrow

--- Contact/contact.php
+++ Contact/contact.php
@@ -1,8 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Post Email</title>
+</head>
+<body>
 <?php
+
 if(!isset($_POST['submit']))
 {
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
+  //This page should not be accessed directly. Need to submit the form.
+  echo "error; you need to submit the form!";
 }
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
@@ -21,18 +29,18 @@ if(IsInjected($visitor_email))
     exit;
 }
 
-$email_from = 'tom@amazing-designs.com';//<== update the email address
+$email_from = ' info@senghorbiz.com.pl';//<== update the email address
 $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
     
-$to = "tom@amazing-designs.com";//<== update the email address
+$to = " info@senghorbiz.com.pl";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
-header('Location: thank-you.html');
+header('Location: ../Home/index.html');
 
 
 // Function to validate against any email injection attempts
@@ -59,3 +67,7 @@ function IsInjected($str)
 }
    
 ?> 
+    
+</body>
+</html>
+
