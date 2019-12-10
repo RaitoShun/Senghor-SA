a0ee7d079c6716110321e43a7943273d5112f7f2
Lolpold-T <leopoldthiao@gmail.com>
2019-12-10 18:44:13 +0200

    responsiveness for mobile

--- Home/index.js
+++ Home/index.js
@@ -4,22 +4,22 @@ const dadInfo = [dadInfoR,dadInfoL];
 
 let scroll = window.requestAnimationFrame ||
             function(callback){ window.setTimeout(callback, 1000/60)};  //Works like the debounce function for a monitor refresh rate of 60.
-            															//Either does fancy new JS or a manual function for this effect
+                                          //Either does fancy new JS or a manual function for this effect
 
-function loop() {	//This function is the one that triggers every function triggering every other, but perfomance is still kept via the scroll variable.
+function loop() { //This function is the one that triggers every function triggering every other, but perfomance is still kept via the scroll variable.
   dadInfo.forEach(function (element) { //Loops through Dad info components, may need to make arrays for each group on the page
     if (isElementInViewport(element)) { //Checks if element is visible
-   		if(element.classList.value.includes('left')){ //Is it a left hidden or right hidden component?
-   			 element.classList.remove('hidden-l'); 
-   		}else{
-   			element.classList.remove('hidden-r');
-   		}
+      if(element.classList.value.includes('left')){ //Is it a left hidden or right hidden component?
+         element.classList.remove('hidden-l'); 
+      }else{
+        element.classList.remove('hidden-r');
+      }
     } else {
-		if(element.classList.value.includes('left')){
-   			 element.classList.add('hidden-l'); 
-   		}else{
-   			element.classList.add('hidden-r');
-   		}
+    if(element.classList.value.includes('left')){
+         element.classList.add('hidden-l'); 
+      }else{
+        element.classList.add('hidden-r');
+      }
     }
   });
 
