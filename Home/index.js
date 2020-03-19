c74e3d4f0fe77868ee331b9221fd19577b5df1e5
RaitoShun <abrahamthiao@gmail.com>
2020-03-19 09:12:10 +0200

    updating new public repo with cleaner,fresh code

--- Home/index.js
+++ Home/index.js
@@ -4,33 +4,29 @@ const dadInfo = [dadInfoR,dadInfoL];
 
 let scroll = window.requestAnimationFrame ||
             function(callback){ window.setTimeout(callback, 1000/60)};  //Works like the debounce function for a monitor refresh rate of 60.
-                                          //Either does fancy new JS or a manual function for this effect
+            															//Either does fancy new JS or a manual function for this effect
 
-function loop() { //This function is the one that triggers every function triggering every other, but perfomance is still kept via the scroll variable.
+function animLoop() {	//This function is the one that triggers every function triggering every other, but perfomance is still kept via the scroll variable.
   dadInfo.forEach(function (element) { //Loops through Dad info components, may need to make arrays for each group on the page
     if (isElementInViewport(element)) { //Checks if element is visible
-      if(element.classList.value.includes('left')){ //Is it a left hidden or right hidden component?
-         element.classList.remove('hidden-l'); 
-      }else{
-        element.classList.remove('hidden-r');
-      }
+   		if(element.classList.value.includes('left')){ //Is it a left hidden or right hidden component?
+   			 element.classList.remove('hidden-l'); 
+   		}else{
+   			element.classList.remove('hidden-r');
+   		}
     } else {
-    if(element.classList.value.includes('left')){
-         element.classList.add('hidden-l'); 
-      }else{
-        element.classList.add('hidden-r');
-      }
+		if(element.classList.value.includes('left')){
+   			 element.classList.add('hidden-l'); 
+   		}else{
+   			element.classList.add('hidden-r');
+   		}
     }
   });
 
-  scroll(loop);
+  scroll(animLoop);
 }
 
 function isElementInViewport(el) {
-  // special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
   var rect = el.getBoundingClientRect(); //Location of the element
   return ( //Does the fancy function of checking in regard to window height and element position whther it is visble currently.
     (rect.top <= 0
@@ -44,4 +40,4 @@ function isElementInViewport(el) {
   );
 }
 
-loop(); //Initiates the functions calling each other
+animLoop(); //Initiates the functions calling each other
